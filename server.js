20ec7bea56d84855addadfd6a98d095a124d890b
sitrakak <shajaniri@gmail.com>
2023-06-15 22:23:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose.Promise = global.Promise;
 //mongoose.set('debug', true);
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
-const uri = 'mongodb+srv://mb:toto@cluster0.5e6cs7n.mongodb.net/assignments?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://sitraka:Sitraka123@cluster0.mgruhxx.mongodb.net/?retryWrites=true&w=majority';
 
 const options = {
   useNewUrlParser: true,
